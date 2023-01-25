927a51845301947db7746315ad69e5a96dd8b202
Marc Grunberg <marc.grunberg@unistra.fr>
2023-01-25 01:07:36 +0100

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    author="Marc Grunberg",
+    license="GNU Lesser General Public License, Version 3 (LGPLv3)",
+    platforms="OS Independent",
+    name="eventfetcher",
+    version="1.0",
+    install_requires=[
+        "obspy >= 1.4.0",
+    ],
+)
