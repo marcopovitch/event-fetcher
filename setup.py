95b7686c43d839b3178a280fc026cc8e4ef01309
Marc Grunberg <marc.grunberg@unistra.fr>
2024-03-24 18:42:17 +0100

    version 1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     license="GNU Lesser General Public License, Version 3 (LGPLv3)",
     platforms="OS Independent",
     name="eventfetcher",
-    version="1.1",
+    version="1.2",
     install_requires=[
         "obspy >= 1.4.0",
         "pyyaml",
